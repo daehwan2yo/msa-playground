033e9a6e977ded97b3d747bb2770e72d98009343
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-18 13:41:50 +0900

    #3 : Product, mongo db 연결 및 환경변수 설정

--- product/src/main/java/com/playground/product/ProductApplication.java
+++ product/src/main/java/com/playground/product/ProductApplication.java
@@ -1,14 +1,23 @@
 package com.playground.product;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.playground", "com.micro"})
 public class ProductApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
+	// 각 micro service 는 자체적으로 디비와 연결되어 전체적으로 봤을때 구분하기 힘드므로,
+	// 각 서비스가 접근하는 database를 로그로 남겨둔다.
+	private static final Logger LOG = LoggerFactory.getLogger(ProductApplication.class);
 
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(ProductApplication.class, args);
+		String mongoDbConnectionUri = applicationContext.getEnvironment()
+														.getProperty("spring.data.mongodb.uri");
+		LOG.info("Connected to MongoDb : {} ", mongoDbConnectionUri);
+	}
 }
