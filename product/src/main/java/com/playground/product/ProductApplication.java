9cf52e33558dea9b5ee442ef2861a45f646ea605
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-22 02:27:17 +0900

    #3 : yml 로 Mongo Config 옮김
    인증 관련 부분이 안돼서 빼고 적용했다.

--- product/src/main/java/com/playground/product/ProductApplication.java
+++ product/src/main/java/com/playground/product/ProductApplication.java
@@ -3,16 +3,9 @@ package com.playground.product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.playground", "com.micro"})
