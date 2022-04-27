6622bbfc99856838f102e6a1de20d51cf92915c8
mac_daehwan2yo <daehwan7989@naver.com>
2022-04-27 16:39:07 +0900

    #5 : Service Discovery 서버 등록 (Eureka)

--- removeAllBuild.sh
+++ removeAllBuild.sh
@@ -2,3 +2,6 @@ sudo rm -r ./product/build
 sudo rm -r ./product-composite/build
 sudo rm -r ./recommendation/build
 sudo rm -r ./review/build
+sudo rm -r ./api/build
+sudo rm -r ./util/build
+sudo rm -r ./discovery/build
